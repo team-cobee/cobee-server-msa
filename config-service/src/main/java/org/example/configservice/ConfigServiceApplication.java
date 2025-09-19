b2654ceeea6b5233f8ccf9739d21a7870fe43bd0
hyejin <hyejin66@sookmyung.ac.kr>
2025-09-19 10:34:43 +0900

    setting: spring cloud 프로젝트 초기 세팅 (#1)

--- config-service/src/main/java/org/example/configservice/ConfigServiceApplication.java
+++ config-service/src/main/java/org/example/configservice/ConfigServiceApplication.java
@@ -1,15 +1,17 @@
-package org.example.common;
+package org.example.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-public class CommonApplication {
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfigServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
+        SpringApplication.run(ConfigServiceApplication.class, args);
     }
 
 }
