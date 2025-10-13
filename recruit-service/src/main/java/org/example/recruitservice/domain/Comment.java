a75887c5fc485d499d8ccc13997985cc35d8868b
hwnooy <heejudgit@gmail.com>
2025-10-14 00:08:29 +0900

    feat: Comment 엔티티 생성

---
+++ recruit-service/src/main/java/org/example/recruitservice/domain/Comment.java
@@ -0,0 +1,43 @@
+package org.example.recruitservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String content;
+
+    @Column
+    private Boolean isPrivate;
+
+    @Column
+    private Long commentAuthorId; // member
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private RecruitPost post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Comment parent;  // 부모 댓글
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> children = new ArrayList<>(); // 대댓글
+
+}
