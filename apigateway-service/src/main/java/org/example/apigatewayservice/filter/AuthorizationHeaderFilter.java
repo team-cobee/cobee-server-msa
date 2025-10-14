56c9bfd123060dc2543b6c31b1f7803e87ec1f5b
LJYeon12 <juyeon010530@gmail.com>
2025-10-15 01:43:12 +0900

    feat: 인가 서비스 구현 완료

---
+++ apigateway-service/src/main/java/org/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,86 @@
+package org.example.apigatewayservice.filter;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Component
+@Slf4j
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    private final SecretKey key;
+
+    public AuthorizationHeaderFilter(@Value("${jwt.secret}") String secretKey) {
+        super(Config.class);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(AuthorizationHeaderFilter.Config config) {
+        // 여기에 필터로직 작성
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+
+            // 1. Authorization 헤더가 있는지 확인
+            if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+            // 2. 헤더에서 토큰 추출
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer ", "");
+
+            // 토큰에서 memberId 추출 & 유효성 검사
+            String memberId = getMemberId(jwt);
+            if (memberId == null) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            // 요청 헤더에 memberId 추가
+            ServerHttpRequest newRequest = request.mutate()
+                    .header("X-USER-ID", memberId)
+                    .build();
+            // 다음 필터로 요청 넘김
+            return chain.filter(exchange);
+        };
+    }
+
+    // JWT를 파싱하여 memberId(subject)를 추출하는 메서드
+    private String getMemberId(String jwt) {
+        try {
+            Claims claims = Jwts.parserBuilder().setSigningKey(key).build()
+                    .parseClaimsJws(jwt).getBody();
+            return claims.getSubject();
+        } catch (Exception e) {
+            log.error("JWT Token is not valid", e);
+            return null;
+        }
+    }
+
+    // 에러 발생 시 클라이언트에게 오류 응답을 보내는 메서드
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        log.error(err);
+        return  response.setComplete();
+    }
+}
