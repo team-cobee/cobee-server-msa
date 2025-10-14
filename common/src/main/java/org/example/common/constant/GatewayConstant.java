f3708bf492cdc6d4aea549f6c66479c5c3831a56
LJYeon12 <juyeon010530@gmail.com>
2025-10-15 01:54:59 +0900

    feat: reqeustHeader용 상수 구현

---
+++ common/src/main/java/org/example/common/constant/GatewayConstant.java
@@ -0,0 +1,10 @@
+package org.example.common.constant;
+
+public final class GatewayConstant {
+
+    // private 생성자로 인스턴스화 방지
+    private GatewayConstant() {
+    }
+
+    public static final String GATEWAY_AUTH_HEADER = "X-USER-ID";
+}
