aa42e86adfcf2ea669cf89aefad51217f5a1c6e6
hwnooy <heejudgit@gmail.com>
2025-10-16 17:48:05 +0900

    fix: develop 브랜치와 머지 후 충돌 해결

--- recruit-service/src/main/java/org/example/recruitservice/controller/CommentController.java
+++ recruit-service/src/main/java/org/example/recruitservice/controller/CommentController.java
@@ -1,7 +1,11 @@
 package org.example.recruitservice.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.common.apiPayload.response.ApiResponse;
+import org.example.recruitservice.domain.Comment;
 import org.example.recruitservice.service.CommentService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    @PostMapping("")
+    public ApiResponse<Comment> addComment(@RequestBody Comment comment) {}
 }
