93913bd81ffeb8dd6d102451c0cb95d586391c75
LJYeon12 <juyeon010530@gmail.com>
2025-10-20 11:43:55 +0900

    feat: ocr 구현 완료

--- member-service/src/main/java/org/example/memberservice/controller/OcrController.java
+++ member-service/src/main/java/org/example/memberservice/controller/OcrController.java
@@ -3,6 +3,7 @@ package org.example.memberservice.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.common.apiPayload.response.ApiResponse;
 import org.example.common.constant.GatewayConstant;
+import org.example.memberservice.dto.MemberInfoDto;
 import org.example.memberservice.service.OcrService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,15 +20,7 @@ public class OcrController {
             @RequestHeader(GatewayConstant.GATEWAY_AUTH_HEADER) Long memberId,
             @RequestParam("image") MultipartFile imageFile
     ){
-/*
-        boolean isSuccess = ocrService.verifyAndUpdateOcrStatus(memberId, imageFile);
-
-        if (isSuccess) {
-            return ApiResponse.success("OCR 인증에 성공했습니다.", "200", null);
-        } else {
-            return ApiResponse.failure("OCR 인증에 실패했습니다.", "400", "정보가 일치하지 않습니다.");
-        }
-*/
-        return null;
+        MemberInfoDto memberInfoDto = ocrService.verify(memberId, imageFile);
+        return ApiResponse.success("신분증 인증 성공", "200", memberInfoDto);
     }
 }
